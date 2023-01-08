a46771615e0f002be45c471ea419ca75964db056
TimeaNagyGarwan <timea.nagy@garwan.sk>
2023-01-08 12:19:57 +0100

    mobile

--- src/app/deal/deal.component.ts
+++ src/app/deal/deal.component.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+type Deal = {
+  price: number,
+  kw: number, 
+  name: string
+}
 
 @Component({
   selector: 'app-deal',
   templateUrl: './deal.component.html'
 })
-export class DealComponent {
+export class DealComponent implements OnInit{
+  deal: Deal = {
+    price: 0,
+    kw: 0,
+    name: ''
+  };
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit(): void {
+    this.httpClient.get("assets/akcia.json").subscribe(data =>{
+      console.log(data);
+      this.deal = data as Deal;
+    })
+  }
 
 }
